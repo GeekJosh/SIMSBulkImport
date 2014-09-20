629d7e1b2ef8ec44d379ebcba6d07d15dcdc3142
Matt Smith <matt@matt40k.co.uk>
2014-09-20 01:57:14 +0100

    v

--- SIMSBulkImport/AssemblyInfo.cs
+++ SIMSBulkImport/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0)")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-GB")]
