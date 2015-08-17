a3bea9f5b93bbff370975b999546c1175529f4d6
Matt Smith <matt@matt40k.co.uk>
2015-08-17 23:15:57 +0100

    Add UserGen libraries
    This is work in progress

---
+++ UserGen/Builder.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace UserGen
+{
+    public class Builder
+    {
+        public enum UserFields
+        {
+            Forename,
+            Surname,
+            AdmissionNo,
+            AdmissionYear,
+            Year,
+            YearOfEntry,
+            RegGroup,
+            SystemId,
+            Increment
+        }
+
+        public bool IsValidExpression
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(_expression))
+                    if (ExpressionContainsIncrement || ExpressionContainsAdmissionNo || ExpressionContainsSystemId)
+                        return true;
+                return false;
+            }
+        }
+
+        public string ExpFieldBuilder(UserFields field)
+        {
+            switch (field)
+            {
+                case UserFields.Forename:
+                case UserFields.Surname:
+                    return "[" + field.ToString() + " (0)]";
+                default:
+                    return "[" + field.ToString() + "]";
+            }
+        }
+
+        public string ExpFieldBuilder(UserFields field, int length)
+        {
+            return "[" + field.ToString() + " (" + length + ")]";
+        }
+
+        public bool ExpressionContainsIncrement
+        {
+            get
+            {
+                return (_expression.Contains(ExpFieldBuilder(UserFields.Increment)));
+            }
+        }
+
+        public bool ExpressionContainsAdmissionNo
+        {
+            get
+            {
+                return (_expression.Contains(ExpFieldBuilder(UserFields.AdmissionNo)));
+            }
+        }
+
+        public bool ExpressionContainsSystemId
+        {
+            get
+            {
+                return (_expression.Contains(ExpFieldBuilder(UserFields.SystemId)));
+            }
+        }
+
+        private string _expression;
+
+        public string SetExpression
+        {
+            set
+            {
+                _expression = value;
+            }
+        }
+
+        public string GetExpressionFromLabel(string value)
+        {
+            string value1;
+            value1 = value.Replace(" ", "");
+            if (value1 == "PersonID") 
+                value1 = "SystemId";
+            UserFields field;
+            if (Enum.TryParse(value1, out field))
+                return ExpFieldBuilder(field);
+            return string.Empty;
+        }
+
+        public string GetDefaultPupilUsernameUDF
+        {
+            get
+            {
+                return "Username";
+            }
+        }
+    }
+}
---
+++ UserGen_UnitTest/Builder_UnitTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserGen;
+
+namespace UserGen_UnitTest
+{
+    [TestClass]
+    public class Builder_UnitTest
+    {
+        [TestMethod]
+        public void IsValidExpression_Invalid_MissingIncrement()
+        {
+            Builder _b = new Builder();
+            _b.SetExpression = "[Forename]";
+            bool result = _b.IsValidExpression;
+            Assert.AreNotEqual(true, result);
+        }
+
+        [TestMethod]
+        public void IsValidExpression_Valid()
+        {
+            Builder _b = new Builder();
+            _b.SetExpression = "[Increment]";
+            bool result = _b.IsValidExpression;
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void IsValidExpression_Invalid_MissingFrontBracket()
+        {
+            Builder _b = new Builder();
+            _b.SetExpression = "Increment]";
+            bool result = _b.IsValidExpression;
+            Assert.AreNotEqual(true, result);
+        }
+
+        [TestMethod]
+        public void IsValidExpression_Invalid_MissingBackBracket()
+        {
+            Builder _b = new Builder();
+            _b.SetExpression = "[Increment";
+            bool result = _b.IsValidExpression;
+            Assert.AreNotEqual(true, result);
+        }
+    }
+}
