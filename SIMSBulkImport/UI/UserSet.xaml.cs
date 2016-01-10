5b83a83092818476c69ef8aa2d2c4d1176ff94d9
Matt Smith <matt@matt40k.uk>
2016-01-10 00:13:13 +0000

    Bit more work
    I've started to add on more of the code for this, however I'll need to
    review and I can't figure out my own logic :(
    TODO review and comment the process

--- SIMSBulkImport/UI/UserSet.xaml.cs
+++ SIMSBulkImport/UI/UserSet.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,16 +19,26 @@ namespace Matt40k.SIMSBulkImport
         private static readonly Logger logger = LogManager.GetCurrentClassLogger();
         private BackgroundWorker bw = new BackgroundWorker();
 
+        private DataTable _pupilData;
+        private DataTable _completeData;
+        private List<string> _users = new List<string>();
+
+        private DateTime queryEnd;
+        private DateTime queryStart;
+        private int recordcount;
+        private int recordupto;
+
         public UserSet()
         {
+            logger.Log(LogLevel.Debug, "Matt40k.SIMSBulkImport.UserSet.UserSet()");
             InitializeComponent();
 
-            dataGrid.DataContext = _completeData;
-
+            dataGrid.DataContext = GetCompleteData;
             dataGrid.Items.Refresh();
-            //Status.Content = "Querying SIMS database - 0%";
+
+            Status.Content = "Querying SIMS database - 0%";
             bw = new BackgroundWorker();
-            //bw.WorkerReportsProgress = true;
+            bw.WorkerReportsProgress = true;
             bw.WorkerSupportsCancellation = true;
             bw.DoWork += bw_DoWork;
             bw.ProgressChanged += bw_ProgressChanged;
@@ -40,14 +51,11 @@ namespace Matt40k.SIMSBulkImport
 
         }
 
-        private DataTable _pupilData;
-        private DataTable _completeData;
-        private List<string> _users = new List<string>();
-
         private DataTable GetPupilData
         {
             get
             {
+                logger.Log(LogLevel.Debug, "Matt40k.SIMSBulkImport.UserSet.GetPupil");
                 if (_pupilData == null)
                     _pupilData = Switcher.SimsApiClass.GetPupilHierarchyData;
                 return _pupilData;
@@ -58,6 +66,7 @@ namespace Matt40k.SIMSBulkImport
         {
             get
             {
+                logger.Log(LogLevel.Debug, "Matt40k.SIMSBulkImport.UserSet.GetCompleteData");
                 if (_completeData == null)
                 {
                     _completeData = new DataTable();
@@ -75,6 +84,7 @@ namespace Matt40k.SIMSBulkImport
 
         public bool SetUsername(string username)
         {
+            logger.Log(LogLevel.Debug, "Matt40k.SIMSBulkImport.UserSet.SetUsername(username)");
             if (!_users.Exists(x => x == username))
             {
                 _users.Add(username);
@@ -85,24 +95,26 @@ namespace Matt40k.SIMSBulkImport
 
         private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            //logger.Log(LogLevel.Debug, "Trace:: Matt40k.SIMSBulkImport.ImportWindow.bw_RunWorkerCompleted()");
+            logger.Log(LogLevel.Debug, "Trace:: Matt40k.SIMSBulkImport.ImportWindow.bw_RunWorkerCompleted()");
             if (e.Cancelled)
             {
-                //this.Status.Content = "Disconnected";
                 logger.Log(LogLevel.Info, "User cancelled");
+                this.Status.Content = "User cancelled";
             }
             else if (!(e.Error == null))
             {
                 logger.Log(LogLevel.Error, "Error - " + e.Error.Message);
+                this.Status.Content = "Error - " + e.Error.Message;
             }
             else
             {
-                //this.Status.Content = "";
+                logger.Log(LogLevel.Error, "Completed");
+                this.Status.Content = "Completed";
 
-                //queryEnd = DateTime.Now;
-                //logger.Log(LogLevel.Info,
-                //    "Querying complete: " + queryStart.ToShortTimeString() + " - " +
-                //    DateTime.Compare(queryEnd, queryStart));
+                queryEnd = DateTime.Now;
+                logger.Log(LogLevel.Info,
+                    "Querying complete: " + queryStart.ToShortTimeString() + " - " +
+                    DateTime.Compare(queryEnd, queryStart));
                 dataGrid.Items.Refresh();
                 //this.dataGrid.IsReadOnly = false;
                 //this.dataGrid.CanUserDeleteRows = true;
@@ -111,13 +123,23 @@ namespace Matt40k.SIMSBulkImport
 
         private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            //Status.Content = e.ProgressPercentage + "%";
-            dataGrid.DataContext = _completeData;
-            dataGrid.Items.Refresh();
+            logger.Log(LogLevel.Debug, "Matt40k.SIMSBulkImport.UserSet.bw_ProgressChanged()");
+            Status.Content = "Querying SIMS database - " + e.ProgressPercentage + "%";
+            // Refreshing the dataGrid causes a crash in .net 4.5, so don't refresh. Version number for 4.5 
+            // is actually 4.0.30319.17626 for some reason
+            // http://stackoverflow.com/questions/12971881/how-to-reliably-detect-the-actual-net-4-5-version-installed
+            Version dotNetVersion = System.Environment.Version;
+            if (dotNetVersion.Major <= 4 && dotNetVersion.Minor <= 0 && dotNetVersion.Build <= 30319 && dotNetVersion.Revision < 17626)
+            {
+                dataGrid.DataContext = _completeData;
+                dataGrid.Items.Refresh();
+            }
         }
 
         private void bw_DoWork(object sender, DoWorkEventArgs e)
         {
+            logger.Log(LogLevel.Debug, "Matt40k.SIMSBulkImport.UserSet.bw_DoWork()");
+            queryStart = DateTime.Now;
             var worker = sender as BackgroundWorker;
             //recordcount = Switcher.PreImportClass.GetImportFileRecordCount;
             //recordupto = 0;
@@ -133,16 +155,16 @@ namespace Matt40k.SIMSBulkImport
             }
             */
             _completeData = GetPupilData;
+            logger.Log(LogLevel.Debug, "Total row count: " + _completeData.Rows.Count);
 
-
-            DataRow[] existingUsers = GetPupilData.Select("IsSet = true");
+            DataRow[] existingUsers = _completeData.Select("IsSet = true");
             foreach (DataRow _dr in existingUsers)
             {
                 bool resultAddExisting = SetUsername(_dr["Username"].ToString());
             }
 
-            DataRow[] newUsers = GetPupilData.Select("IsSet = true");
-            foreach (DataRow _dr in _pupilData.Rows)
+            DataRow[] newUsers = _completeData.Select("IsSet = true");
+            foreach (DataRow _dr in newUsers)
             {
                 string newUser;
                 bool addNewOk;
