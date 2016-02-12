e4f118e95121978c915d468f7b6d63635709fb55
Matt Smith <matt@matt40k.co.uk>
2016-02-12 22:16:14 +0000

    Add installer to main repo

---
+++ Installer/AddFiles.ps1
@@ -0,0 +1,14 @@
+﻿$filehead = "<?xml version=""1.0"" encoding=""UTF-8""?>`n<Wix xmlns=""http://schemas.microsoft.com/wix/2006/wi"">`n   <Fragment>`n        <ComponentGroup Id=""SIMSBulkImportComponents"" Directory=""INSTALLDIR"">`n"
+
+$filecontent = ""
+
+$fileEntries = Get-ChildItem "Installer\src"
+foreach($fileName in $fileEntries) 
+{ 
+	$cleanFilename = $fileName -replace " ", ""
+    $filecontent = $filecontent + "`n           <Component>`n               <File Id=""$cleanFilename"" Source=""src\$fileName"" />`n           </Component>"
+}      
+
+$filefoot = "`n`n		</ComponentGroup>`n	</Fragment>`n</Wix>"
+
+$file = $filehead + $filecontent + $filefoot | Out-File Installer\SimsBulkImport.wxs -Encoding "UTF8"
---
+++ Installer/SetVersion.ps1
@@ -0,0 +1,11 @@
+﻿$buildname = $args[0]
+Write-Host $buildname
+$buildPts = $buildname.Split('.')
+
+$majorVer = $buildPts[0]
+$minorVer = $buildPts[1]
+$buildVer = $buildPts[2]
+
+$outPut = "<?xml version=""1.0"" encoding=""utf-8""?>`n<Include>`n	<?define ProductName=""SIMS Bulk Import""?>`n	<?define ProductVersion.Major="""+$majorVer+"""?>`n	<?define ProductVersion.Minor="""+$minorVer+"""?>`n	<?define ProductVersion.Build="""+$buildVer+"""?>`n</Include>"
+Write-Host $outPut
+$outPut | Out-File Installer\Version.wxi -Encoding "UTF8"
